2f4b9b7c39d52a5958f956d5fca43e9125a52a32
ST10083941 <96740648+cgov-0406@users.noreply.github.com>
2023-09-27 12:04:53 +0200

    Added timer

--- ST10083941_PROG7312_POE/MainWindow.xaml.cs
+++ ST10083941_PROG7312_POE/MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,13 +24,23 @@ namespace ST10083941_PROG7312_POE
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<string> CallNumbers = new();
-        CallNumberService CallNumberService = new();
+        private ObservableCollection<string> CallNumbers;
+        private CallNumberService CallNumberService;
+        private Stopwatch Stopwatch;
+        private Timer Timer;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            CallNumberService = new();
+            CallNumbers = new();
             CallNumbers = CallNumberService.GenerateCallNumbers();
+            Stopwatch = new();
+            Timer = new(1000);
+            Timer.Elapsed += OnTimerElapse!;
+
+
             lsvCallNumbers.ItemsSource = CallNumbers;
             lsvCallNumbers.SelectedIndex = 0;
         }
@@ -59,8 +71,29 @@ namespace ST10083941_PROG7312_POE
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            ObservableCollection<string> orderedCallNumbers = (ObservableCollection<string>)CallNumbers.OrderBy(x => x);
+            Stopwatch.Stop();
+            Timer.Stop();
 
+        }
+
+        private void OnTimerElapse(object sender, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => tblTimer.Text = Stopwatch.Elapsed.ToString(@"hh\:mm\:ss"));
+        }
+        private void BeginTimer()
+        {
+            Stopwatch.Start();
+            Timer.Start();
+            CallNumbers = CallNumberService.GenerateCallNumbers();
+            lsvCallNumbers.ItemsSource = CallNumbers;
+            lsvCallNumbers.SelectedIndex = 0;
+        }
+
+        private void btnBegin_Click(object sender, RoutedEventArgs e)
+        {
+            BeginTimer();
+            btnBegin.IsEnabled = false;
+            btnSubmit.IsEnabled = true;
         }
     }
 }
