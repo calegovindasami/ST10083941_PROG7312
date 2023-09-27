3650f2252898a6bf0da92b3042dcc2aab8e7e635
ST10083941 <96740648+cgov-0406@users.noreply.github.com>
2023-09-27 02:07:53 +0200

    Add project files.

---
+++ ST10083941_PROG7312_POE/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ST10083941_PROG7312_POE.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ST10083941_PROG7312_POE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> CallNumbers = new();
+        CallNumberService CallNumberService = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CallNumbers = CallNumberService.GenerateCallNumbers();
+            lsvCallNumbers.ItemsSource = CallNumbers;
+            lsvCallNumbers.SelectedIndex = 0;
+        }
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            var currentIndex = lsvCallNumbers.SelectedIndex;
+            if (currentIndex > 0)
+            {
+                var currentCallNumber = CallNumbers[currentIndex];
+                CallNumbers.RemoveAt(currentIndex);
+                CallNumbers.Insert(currentIndex - 1, currentCallNumber);
+                lsvCallNumbers.SelectedIndex = currentIndex - 1;
+            }
+        }
+
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            var currentIndex = lsvCallNumbers.SelectedIndex;
+            if (currentIndex + 1 < lsvCallNumbers.Items.Count)
+            {
+                var currentCallNumber = CallNumbers[currentIndex];
+                CallNumbers.RemoveAt(currentIndex);
+                CallNumbers.Insert(currentIndex + 1, currentCallNumber);
+                lsvCallNumbers.SelectedIndex = currentIndex + 1;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<string> orderedCallNumbers = (ObservableCollection<string>)CallNumbers.OrderBy(x => x);
+
+        }
+    }
+}
---
+++ ST10083941_PROG7312_POE/Services/CallNumberService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10083941_PROG7312_POE.Services
+{
+    public class CallNumberService
+    {
+        private Random Random;
+
+        public CallNumberService()
+        {
+            Random = new();
+        }
+        private string GenerateClassificationNumber()
+        {
+            int randomNumber = Random.Next(0, 1000);
+            return randomNumber.ToString("D3");
+        }
+        private string GenerateAuthor()
+        {
+            char[] author = new char[3];
+            for (int i = 0; i < 3; i++)
+            {
+                author[i] = (char) Random.Next('A', 'Z' + 1);
+            }
+            return new string(author);
+        }
+        private string GenerateSpecificationNumber()
+        {
+            return Random.Next(0, 1000).ToString("D3");
+        }
+
+
+        public string GenerateCallNumber()
+        {
+            return $"{GenerateClassificationNumber()}.{GenerateSpecificationNumber()} {GenerateAuthor()}";
+        }
+        public ObservableCollection<string> GenerateCallNumbers()
+        {
+            var callNumbers = new ObservableCollection<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                callNumbers.Add(GenerateCallNumber());
+            }
+            return callNumbers;
+        }
+    }
+}
